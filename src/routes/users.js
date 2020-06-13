01fed7ff2f86356994b824257d425588af3d7fd1
Rauf <ar.rauf@gmail.com>
2020-06-13 18:12:25 +0530

    push

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,13 +2,8 @@
 var express = require('express')
 var router = express.Router()
 
-// define the home page route
 router.get('/', function (req, res) {
     res.send('Users home page')
-})
-// define the about route
-router.get('/about', function (req, res) {
-    res.send('About users')
 })
 
 module.exports = router
